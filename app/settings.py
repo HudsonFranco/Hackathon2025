03b806c3fbf2e628e000b5220452cf361973146b
HudsonFranco <hudsonfelipe123@gmail.com>
2025-12-04 13:02:41 -0300

    Adiciona URL do Render no ALLOWED_HOSTS

--- app/settings.py
+++ app/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-i6^ynvsl5@9jww^ekht%d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,hackathon2025-t18q.onrender.com').split(',')
 
 
 # Application definition
